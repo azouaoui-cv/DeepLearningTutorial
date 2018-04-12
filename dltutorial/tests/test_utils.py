61dce0a24c5be9f08164be0bb5aad918acea6606
inzouzouwetrust <alexandre.zouaoui@free.fr>
2018-04-12 17:49:12 +0200

    Added IMDB utility functions tests

--- dltutorial/tests/test_utils.py
+++ dltutorial/tests/test_utils.py
@@ -12,12 +12,16 @@ import os
 
 # Movies database API packages
 import tmdbsimple as tmdb
+import imdb
 
 # Custom utility functions to be tested
 from dltutorial.utils import get_movie_id_tmdb
 from dltutorial.utils import get_movie_info_tmdb
 from dltutorial.utils import get_movie_genres_tmdb
 from dltutorial.utils import get_api_key_tmdb
+
+from dltutorials.utils import get_movie_info_imdb
+from dltutorials.utils import get_movie_genres_imdb
 
 
 
@@ -33,7 +37,7 @@ def test_utils_tmdb(test_movie_name="The Matrix"):
         
     Returns
     --------------
-    None (using by pytest)
+    None (used by pytest)
     """
     
     # Retrieving API Key
@@ -54,3 +58,27 @@ def test_utils_tmdb(test_movie_name="The Matrix"):
     assert isinstance(movie_info, dict)
     movie_genres = get_movie_genres_tmdb(test_movie_name, search_tmdb=search_tmdb)
     assert isinstance(movie_genres, list)
+
+def test_utils_imdb(test_movie_name="The Matrix"):
+    """
+    Unit testing imdb utility functions
+    
+    Parameters
+    --------------
+    - test_movie_name : string (optional)
+        Movie name on which tests are run.
+        By default: 'The Matrix'
+        
+    Returns
+    --------------
+    None (used by pytest)
+    """ 
+    
+    # Instanciate a search object from IMDB
+    search_imdb = imdb.IMDb()
+    
+    # Unit tests
+    movie_genres = get_movie_genres_imdb(movie_name=movie_name, search_imdb=search_imdb)
+    assert isinstance(movie_genres, list)
+    movie_info = get_movie_info_imdb(movie_name=movie_name, search_imdb=search_imdb)
+    assert isinstance(movie_info, imdb.Movie.Movie)
